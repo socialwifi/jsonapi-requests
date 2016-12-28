af3100988a6dd24b96683610a4e9c12f24bd5b2b
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-12-28 13:25:41 +0100

    Add relations to orm.

--- jsonapi_requests/orm/registry.py
+++ jsonapi_requests/orm/registry.py
@@ -17,3 +17,17 @@ class TypeRegistry:
         if type in self.registry:
             raise OrmException('Api already has model of this type')
         self.registry[type] = model
+
+    def get_mapped_orm_objects(self, raw_objects):
+        mapping = {}
+        for raw_object in raw_objects:
+            object = self.get_orm_object(raw_object)
+            mapping[ObjectKey(object.type, object.id)] = object
+        return mapping
+
+    def get_orm_object(self, raw_object):
+        model = self.get_model(raw_object.type)
+        return model(raw_object)
+
+    def get_model(self, type):
+        return self.registry[type]
--- tests/test_orm.py
+++ tests/test_orm.py
@@ -9,12 +9,13 @@ class TestApiModel:
             pass
 
     def test_refresh(self):
-        mock_api = mock.Mock()
-        mock_api.endpoint.return_value.get.return_value.data = mock.Mock(attributes={'name': 'alice'})
+        mock_api = mock.MagicMock()
+        mock_api.endpoint.return_value.get.return_value.content.data = mock.Mock(attributes={'name': 'alice'})
+        orm_api = orm.OrmApi(mock_api)
 
         class Test(orm.ApiModel):
             class Meta:
-                api = mock_api
+                api = orm_api
                 type = 'test'
             name = orm.AttributeField(source='name')
 
@@ -22,3 +23,23 @@ class TestApiModel:
         test.refresh()
         mock_api.endpoint.assert_called_with('test/123/')
         assert test.name == 'alice'
+
+    def test_refresh_with_relationships(self):
+        mock_api = mock.Mock()
+        mock_api.endpoint.return_value.get.return_value.content = mock.MagicMock(
+            data=mock.Mock(relationships={'other': mock.Mock(data=mock.Mock(id='1', type='test'))}),
+            included=[mock.MagicMock(id='1', type='test', attributes={'name': 'alice'})]
+        )
+        orm_api = orm.OrmApi(mock_api)
+
+        class Test(orm.ApiModel):
+            class Meta:
+                api = orm_api
+                type = 'test'
+            other = orm.RelationField(source='other')
+            name = orm.AttributeField(source='name')
+
+        test = Test.from_id('123')
+        test.refresh()
+
+        assert test.other.name == 'alice'
