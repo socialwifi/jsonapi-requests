303b00a1c491b047851f54c060887e6b62cb3f9d
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2020-01-13 12:50:44 +0100

    Fix pip parse requirements import.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from pip.req import parse_requirements
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 from setuptools import find_packages
 from setuptools import setup
 
